69bf133ad428ca69d03075983d1e0207a686825d
성유진 <syj010820@naver.com>
2023-09-04 17:24:15 +0900

    maybedo CRUD

---
+++ src/test/java/project/maybedo/MaybedoApplicationTests.java
@@ -0,0 +1,13 @@
+package project.maybedo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaybedoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
